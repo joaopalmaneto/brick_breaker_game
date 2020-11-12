d0e1772833ba0727d81f6b556a23afd92a58d56c
Joao Palma <jpalmaneto@gmail.com>
2020-11-12 01:10:24 -0300

    estrategia de movimentação das divs e organização do HTML alterados

--- js/jogo.js
+++ js/jogo.js
@@ -1,19 +1,64 @@
-document.onkeydown = detectKey;
+const pad = document.querySelector('#game .area:nth-child(1) div');
+const area = document.querySelector('#game');
 
-function detectKey(e){
-    posCima = document.getElementById("pad").offsetTop;
+window.addEventListener('keydown', moverPad);
 
-    console.log(posCima);
+function moverPad(event){
+    if(event.key === "ArrowUp"){
+        event.preventDefault();
+        moveUp();   
+    } else if(event.key === "ArrowDown"){
+        event.preventDefault();
+        moveDown();
+    } else if(event.key === "SpaceBar") {
 
-    e = e || window.Event;
-    if(e.keyCode == '40' && posCima <= 456){
-
-        //desce
-        document.getElementById("pad").style.top = (posCima + 20) + "px";
     }
-    else if(e.keyCode == '38' && posCima > 110){
+}
 
-        //sobe
-        document.getElementById("pad").style.top = (posCima - 20) +  "px";
+function moveUp(){
+    let topPos = getComputedStyle(pad).getPropertyValue("top");
+    if(topPos === "0px"){
+        return
+    } else {
+        topPos = parseInt(topPos);
+        topPos -= 10;
+        pad.style.top = `${topPos}px`;
     }
+}
+
+function moveDown(){
+    let topPos = getComputedStyle(pad).getPropertyValue("top");
+    if(topPos === "360px"){
+        return
+    } else {
+        topPos = parseInt(topPos);
+        topPos += 10;
+        pad.style.top = `${topPos}px`;
+    }
+}
+
+function ballController(){
+    ball = createBall();
+    area.appendChild(ball);
+    moveBall(ball);
+}
+
+function createBall(){
+    let xPos = parseInt(getComputedStyle(pad).getPropertyValue("left"));
+    let yPos = parseInt(getComputedStyle(pad).getPropertyValue("top"));
+    let ball = document.createElement("div");
+
+    ball.classList.add("ball");
+    ball.style.top = `${yPos - 20}px`;
+    ball.style.left = `${xPos}px`;
+
+    return ball;
+}
+
+function moveBall(ball){
+    let posSetter = setInterval(() => {
+        let yPos = getComputedStyle(ball).getPropertyValue("top");
+        let xPos = getComputedStyle(ball).getPropertyValue("left");
+    }
+    , 10)
 }
